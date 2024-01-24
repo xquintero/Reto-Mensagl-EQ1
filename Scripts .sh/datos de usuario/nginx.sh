6ae55522e0a840343f5b188d011898625f96ff57
sergio-diazz <155975588+sergio-diazz@users.noreply.github.com>
2024-01-24 10:51:53 +0100

    Delete Scripts .sh directory

--- Scripts .sh/datos de usuario/nginx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install nginx -y
-apt-get install python3-certbot-nginx -y
--- Scripts .sh/pruebas/postgre.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | tee /usr/share/keyrings/postgresql-key.gpg >/dev/null
-sh -c 'echo "deb [signed-by=/usr/share/keyrings/postgresql-key.gpg arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-apt update -y
-apt install postgresql postgresql-contrib -y
